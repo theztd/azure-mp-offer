c1e16b034e65922fc9f89971743046cd8163cac2
Marek Sirovy <msirovy@gmail.com>
2021-02-10 16:40:24 +0100

    init

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Deployment of your app has been started..."
+
+echo "Deployment uses this variables..."
+env
+
+
+echo "Install application..."
+sudo -i ruby /opt/easy_cloud_maintenance/bin/easy_cloud deployment create azure.testvm.com \
+  --repository=${REPO} \
+  --branch=master \
+  --source_dump_url=${DB_URL} \
+  --source_files_url=${FILES_URL} \
+  --listen-on-ip \
+  --no-redirect-to-https \
+  --ssl-certificate=false \
+  --app-home /home/easy
